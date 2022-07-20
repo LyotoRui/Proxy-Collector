bec7925b60083cd3cf160ff8ed3703cdd6759f23
LyotoRui <80151467+LyotoRui@users.noreply.github.com>
2022-07-20 12:26:03 +0300

    added api interface

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask
+
+from routes.api import api
 from routes.index import main
 
+
 def register_blueprints(app: Flask) -> None:
+    main.register_blueprint(api)
     app.register_blueprint(main)
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.config.from_pyfile('config.py')
+    app.config.from_pyfile("config.py")
     register_blueprints(app=app)
     return app
 
+
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
