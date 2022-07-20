6100dbee2058b7f93748725ebc2117acc6d5c85d
LyotoRui <80151467+LyotoRui@users.noreply.github.com>
2022-07-20 12:32:40 +0300

    updated requirements

--- routes/index.py
+++ routes/index.py
@@ -1,23 +1,27 @@
 from black import main
+from collector import Collector
 from flask import Blueprint, jsonify, render_template, request
 from models import Countries
-from collector import Collector
 
-main = Blueprint('index', __name__)
+main = Blueprint("index", __name__)
 
-@main.route('/', methods=['GET', 'POST'])
+
+@main.route("/", methods=["GET", "POST"])
 def index():
     match request.method:
-        case 'POST':
-            request_data = request.get_json()['data']
-            print(request_data)
+        case "POST":
+            request_data = request.get_json()["data"]
             response_data = Collector.do_work(
-                countries=request_data['countries'] if len(request_data['countries']) else ['US'],
-                types=request_data['types'] if len(request_data['types']) else ['HTTP'],
-                anon=request_data['anon'] if len(request_data['anon']) else ['HIGH'],
-                format=request_data['format'] if len(request_data['format']) == 1 else ['json']
+                countries=request_data["countries"] if len(request_data["countries"]) else ["US"],
+                types=request_data["types"] if len(request_data["types"]) else ["HTTP"],
+                anon=request_data["anon"] if len(request_data["anon"]) else ["HIGH"],
+                format=request_data["format"] if len(request_data["format"]) == 1 else ["json"],
             )
-            print(response_data)
             return jsonify(response_data)
-        case 'GET':
-            return render_template('index.html', countries=zip(Countries.get_list_of_names(), Countries.get_list_of_values()))
+        case "GET":
+            return render_template(
+                "index.html",
+                countries=zip(
+                    Countries.get_list_of_names(), Countries.get_list_of_values()
+                ),
+            )
